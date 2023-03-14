f9e13cbd4e47379b856ab7bea02e5240c2df0924
Akshay@2003 <akshay88magar@gmail.com>
2023-03-14 19:37:23 +0530

    Getdata function implemented

--- app/src/main/java/com/example/memes/ApiInterface.kt
+++ app/src/main/java/com/example/memes/ApiInterface.kt
@@ -1,8 +1,9 @@
 package com.example.memes
 
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiInterface {
     @GET("/gimme")
-    fun getData()
+    fun getData(): Call<responseDataClass>
 }
--- app/src/main/java/com/example/memes/MainActivity.kt
+++ app/src/main/java/com/example/memes/MainActivity.kt
@@ -1,13 +1,41 @@
 package com.example.memes
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        getData()
 //        https://meme-api.com/gimme
+    }
+
+    private fun getData() {
+
+        val progressDialog=ProgressDialog(this)
+        progressDialog.setMessage("Please wait while data is fetch")
+        progressDialog.show()
+
+
+       RetrofitInstance.apiInterface.getData().enqueue(object : Callback<responseDataClass?>{
+        override fun onResponse(
+            call: Call<responseDataClass?>,response: Response<responseDataClass?>
+        ){
+             progressDialog.dismiss()
+        }
+
+           override fun onFailure(call: Call<responseDataClass?>, t: Throwable) {
+              Toast.makeText(this@MainActivity,"${t.localizedMessage}",Toast.LENGTH_SHORT).show()
+                progressDialog.dismiss()
+           }
+
+
+       })
     }
 }
